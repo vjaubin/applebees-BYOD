15b5b3720eaf9cf355c9cfff80d56b7ef33c900e
Vincent Jaubin <vjaubin@gmail.com>
2019-05-15 07:06:38 -0700

    Edit

--- js/global.js
+++ js/global.js
@@ -1,6 +1,6 @@
     $(document).ready(function(){
         
-        $('.credit-card').mask('0000-0000-0000-0000');
+        $('.credit-card').mask('0000 0000 0000 0000');
         $('.exp-date').mask('00/00');
         $('.CCV').mask('0000');
         $('.zip-code').mask('00000');
