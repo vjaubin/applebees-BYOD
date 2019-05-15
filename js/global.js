29550cb8c41c9b7cfec3d2e910e5da1a61ab4227
Vincent Jaubin <vjaubin@gmail.com>
2019-05-14 21:06:59 -0700

    Edit

---
+++ js/global.js
@@ -0,0 +1,13 @@
+    $(document).ready(function(){
+        
+        $('.credit-card').mask('0000-0000-0000-0000');
+        $('.exp-date').mask('00/00');
+        $('.CCV').mask('0000');
+        $('.zip-code').mask('00000');
+        $('.pin').mask('00000');
+        
+        
+        
+
+        
+    });
